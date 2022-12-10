8d36ca839e87440253762f028ec3f34158ec73a6
p-real <developer.pjr@gmail.com>
2022-12-10 11:22:39 -0800

    cleaning up script

--- install.bash
+++ install.bash
@@ -1,24 +1,52 @@
 #!/bin/bash
 
+# Check for required dependencies
+dependencies=(curl qemu-system-x86_64 cloud-localds)
+for dep in "${dependencies[@]}"; do
+    if ! command -v "$dep" > /dev/null 2>&1; then
+        echo "Error: $dep is not installed" >&2
+        exit 1
+    fi
+done
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -m|--memory)
+            VM_MEMORY="$2"
+            shift
+            shift
+            ;;
+        -p|--port)
+            VM_SSH_PORT="$2"
+            shift
+            shift
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
+# Use default values if not set
+VM_MEMORY=${VM_MEMORY:-4096M}
+VM_SSH_PORT=${VM_SSH_PORT:-5511}
+VM_UUID="6ba7b810-9dad-11d1-80b4-00c04fd430c8"
 
 URL=https://cloud.debian.org/images/cloud/bullseye/latest
 FILE=debian-11-generic-amd64.raw
 BACKING_FILE=debian-11-generic-amd64-backing_file.raw
-SHA_512=e643682e0b014dbabc6d13959f25e43b3c9e8ad5a409d0659147127be99fd3b80deadcb696774a9be26f9ca15b6d52ae44a7691c5b0467b786141e4d3a80254a
 
+SHA_512=$(curl -sL "$URL/SHA512SUMS" | grep "$FILE" | awk '{print $1}')
 
 # Cloud-init config
 VM_INSTANCE=debian-001
-VM_HOST=debian
+VM_HOST=devbox
 SEED_IMAGE_FILE=seed.img 
 USER_DATA_FILE=user-data.yaml
 META_DATA_FILE=meta-data.yaml
 SSH_KEY_NAME=id_rsa
-
-# QEMU config
-VM_SSH_PORT=5511
-VM_MEMORY=4096M
-
 
 if [ -f "$FILE" ]; then
     echo "$FILE exists locally, don't download"
@@ -67,9 +95,10 @@ echo "ssh -i $SSH_KEY_NAME -p $VM_SSH_PORT debian@localhost"
 qemu-system-x86_64 \
     -device "virtio-net,netdev=user.0" \
     -machine "type=q35,accel=kvm" \
-    -m "$VM_MEMORY" \
     -boot "c" \
-    -name "debiantest" \
+    -m "$VM_MEMORY" \
+    -mem-prealloc \
+    -uuid "$VM_UUID" \
     -netdev "user,id=user.0,hostfwd=tcp::$VM_SSH_PORT-:22" \
     -drive file=$PWD/$BACKING_FILE,if=virtio,cache=writeback,discard=ignore,format=raw \
     -drive if=virtio,format=raw,file=$SEED_IMAGE_FILE
